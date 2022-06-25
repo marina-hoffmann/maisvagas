3a70789727d3671689f20b5ac479356e18535c55
marina-hoffmann <marina.hoffmann@aurum.com.br>
2022-06-24 22:50:53 -0300

    criando interface de edicao, criacao e outros ajustes

--- vagas/urls.py
+++ vagas/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('criar-vaga', views.criar_vaga, name='criar-vaga')
+    path('criar-vaga', views.criar_vaga, name='criar-vaga'),
+    path('editar-vaga/<int:vaga_id>', views.editar_vaga, name='editar-vaga'),
+    path('deletar-vaga', views.deletar_vaga, name='deletar-vaga'),
+    path('vaga/<int:id_vaga>', views.vaga, name='vaga')
 ]
--- vagas/views.py
+++ vagas/views.py
@@ -1,22 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from vagas.models import Vaga
+from django.contrib.auth.models import User
+from django.utils.datastructures import MultiValueDictKeyError
+from django.contrib import messages
 
 def index(request):
     vagas = Vaga.objects.all()
     return render(request, 'vagas.html', context={'vagas':vagas})
 
 def criar_vaga(request):
-    # if request.method == 'POST':
-    #     nome_receita = request.POST['nome_receita']
-    #     ingredientes = request.POST['ingredientes']
-    #     modo_preparo = request.POST['modo_preparo']
-    #     tempo_preparo = request.POST['tempo_preparo']
-    #     rendimento = request.POST['rendimento']
-    #     categoria = request.POST['categoria']
-    #     foto_receita = request.FILES['foto_receita']
-    #     user = get_object_or_404(User, pk=request.user.id)
-    #     receita = Receita.objects.create(pessoa=user,nome_receita=nome_receita, ingredientes=ingredientes, modo_preparo=modo_preparo,tempo_preparo=tempo_preparo, rendimento=rendimento,categoria=categoria, foto_receita=foto_receita)
-    #     receita.save()
-    #     return redirect('dashboard')
-    # else:
-    return render(request, 'criar-vaga.html')
+    if request.method == 'POST':
+        titulo = request.POST['titulo']
+        local = request.POST['local']
+        descricao = request.POST['descricao']
+        requisitos = request.POST['requisitos']
+        beneficios = request.POST['beneficios']
+        categoria = request.POST['categoria']
+        senioridade = request.POST['senioridade']
+        user = get_object_or_404(User, pk=request.user.id)
+        try:    
+            publicada = request.POST['publicada']
+            publicada = True                    
+        except MultiValueDictKeyError:
+            publicada = False
+        vaga = Vaga.objects.create(empresa=user,titulo=titulo,local=local,descricao=descricao, requisitos=requisitos,beneficios=beneficios, publicada=publicada, categoria=categoria, senioridade=senioridade)
+        vaga.save()
+        return redirect('minhas-vagas')
+    else:
+        return render(request, 'criar-vaga.html')
+
+def deletar_vaga(request):
+    pass
+
+def editar_vaga(request, vaga_id):    
+    vaga = Vaga.objects.filter(id=vaga_id).first()
+    dados = {'vaga' : vaga}
+    if request.method == 'POST':
+        vaga.titulo = request.POST['titulo']
+        vaga.local = request.POST['local']
+        vaga.descricao = request.POST['descricao']
+        vaga.requisitos = request.POST['requisitos']
+        vaga.beneficios = request.POST['beneficios']
+        vaga.categoria = request.POST['categoria']
+        vaga.senioridade = request.POST['senioridade']
+        vaga.user = get_object_or_404(User, pk=request.user.id)
+        try:    
+            publicada = request.POST['publicada']
+            vaga.publicada = True                    
+        except MultiValueDictKeyError:
+            vaga.publicada = False
+        vaga.save()
+        return redirect('minhas-vagas')
+    return render(request, 'editar-vaga.html', dados)
+
+def vaga(request, id_vaga):
+    vaga = Vaga.objects.filter(id=id_vaga).first()
+    if (not vaga == None):
+        return render(request, 'vaga.html', context={"vaga":vaga})
+    else:
+        messages.error(request,'Vaga não encontrada.')
+        return redirect('/')
